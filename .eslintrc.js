48977a8e6d8003ee7e91f0272861bfbc0fc18777
Aubrey Taylor <aubricus@gmail.com>
2022-10-24 16:30:46 -0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+// See: https://eslint.org/docs/user-guide/configuring/configuration-files
+
+const isDevelopment = process.env.NODE_ENV === "development";
+const OFF = "off";
+
+module.exports = {
+  extends: ["eslint:recommended", "prettier", "plugin:vue/vue3-essential"],
+  plugins: ["prettier", "vue"],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    commonjs: true,
+  },
+  /**
+   * Define globals to solve '[GLOBAL]' is not define – no undef
+   */
+  globals: {
+    Shopify: "readonly",
+  },
+  /**
+   * add custom rules
+   * docs: https://eslint.org/docs/rules
+   */
+  rules: {
+    "prettier/prettier": "error",
+  },
+  overrides: [],
+
+  /**
+   * ignore certain files
+   * docs: https://eslint.org/docs/user-guide/configuring#ignorepatterns-in-config-files
+   */
+  ignorePatterns: [
+    "shopify/assets/vendor-scripts-v11.js",
+    "shopify/assets/bundle.js",
+    "shopify/assets/checkout.js",
+    // Allows a simpler glob in eslintrc.js
+    "shopify/assets/*.vue",
+  ],
+};
---
+++ .stylelintrc.js
@@ -0,0 +1,29 @@
+// See: https://stylelint.io/user-guide/configure
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
+module.exports = {
+  extends: ["stylelint-config-recommended", "stylelint-config-prettier"],
+  /**
+   * add plugins
+   * docs: https://stylelint.io/user-guide/configure#plugins
+   */
+  plugins: [
+    // 'stylelint-scss' // stylelint by itself supports SCSS syntax very well
+  ],
+  /**
+   * ignore certain files
+   * docs: https://stylelint.io/user-guide/configure#ignorefiles
+   */
+  ignoreFiles: [
+    "shopify/assets/bundle.css",
+    "shopify/assets/checkout.css"
+  ],
+  /**
+   * add custom rules
+   * docs: https://stylelint.io/user-guide/rules/list
+   */
+  rules: {},
+  overrides: [],
+  customSyntax: "postcss-html",
+};
