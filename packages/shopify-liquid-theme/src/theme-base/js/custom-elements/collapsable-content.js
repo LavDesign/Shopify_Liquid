8336e780e5458c7c11c604e808a362c372cc0a90
omid.habedy <omid.habedy@amardydrwjyyf.attlocal.net>
2023-01-30 15:11:09 -0600

    tabs in progress

--- packages/shopify-liquid-theme/src/theme-base/js/custom-elements/collapsable-content.js
+++ packages/shopify-liquid-theme/src/theme-base/js/custom-elements/collapsable-content.js
@@ -2,35 +2,76 @@ class CollapsableContent extends HTMLElement {
   constructor() {
     super();
     this.tabItems = this.querySelectorAll(".collapsable-item");
-    this.toggleAccordion = this.toggleAccordion.bind(this);
+    this.tabToggleBtns = this.querySelectorAll(".collapsable-toggle");
+    this.tabContents = this.querySelectorAll(".collapsable-content");
+    this.isStacked = this.getAttribute("data-stacked");
+    this.windowsWidth = window.innerWidth;
   }
 
   connectedCallback() {
-    this.render();
     if (this.tabItems === null || this.tabItems === undefined) return;
+    this.stacked();
+    this.render();
+  }
 
-    this.tabItems.forEach((item) => {
-      item.setAttribute("data-toggled", false);
-      item
-        .querySelector("button.collapsable-toggle")
-        .addEventListener("click", () => {
-          this.toggleTab(item.querySelector("button.collapsable-toggle"));
-        });
+  render() {
+    this.toggleTab();
+  }
+
+  // default stack on mobile, desktop if [data-stacked="true]
+  stacked() {
+    console.log("isStacked", this.isStacked);
+
+    if (this.isStacked == "false" || this.windowsWidth > 768) {
+      const btnWrapper = document.createElement("div");
+      btnWrapper.classList.add("collapsable-buttons");
+
+      this.tabToggleBtns.forEach((button) => {
+        const clone = button.cloneNode(true);
+        btnWrapper.prepend(clone);
+        this.prepend(btnWrapper);
+        button.remove();
+      });
+      this.tabToggleBtns = this.querySelectorAll(".collapsable-toggle");
+    }
+  }
+
+  // set active collapsable tab
+  setActiveTab(button) {
+    button.setAttribute("data-toggled", true);
+
+    this.tabContents.forEach((content) => {
+      const buttonId = button.getAttribute("data-toggle-id");
+      const collapsableId = content.getAttribute("id");
+      const contentHeight = content.querySelector(
+        ".collapsable-content__inner"
+      ).offsetHeight;
+
+      console.log(contentHeight);
+
+      // set active - found matching collapsable content
+      if (collapsableId == buttonId) {
+        content.setAttribute("data-expanded", true);
+        content.style.height = `${contentHeight}px`;
+      } else {
+        content.setAttribute("data-expanded", false);
+        content.style.height = "0px";
+      }
     });
   }
 
-  toggleAccordion() {}
+  // attach event listener to toggle buttons
+  toggleTab() {
+    this.tabToggleBtns.forEach((button) => {
+      button.setAttribute("data-toggled", false);
 
-  render() {
-    console.log(this.tabItems);
-  }
+      button.addEventListener("click", () => {
+        console.log(button);
+        this.setActiveTab(button);
+      });
+    });
 
-  toggleTab(button) {
-    console.log(button);
-    const toggled = Boolean(button.getAttribute("data-toggled"));
-    button.setAttribute("data-toggled", true);
-
-    console.log(toggled);
+    console.log("toggleTab");
   }
 }
 
