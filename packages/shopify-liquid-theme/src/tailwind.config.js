bf574f13f022ca7ff59223f39c2bfc43301ba053
omid.habedy <omid.habedy@amardydrwjyyf.attlocal.net>
2023-02-02 16:36:13 -0600

    bug fixed

--- packages/shopify-liquid-theme/src/tailwind.config.js
+++ packages/shopify-liquid-theme/src/tailwind.config.js
@@ -5,7 +5,7 @@
  * default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 const path = require("path");
-const bedrockConfig = require("../bedrock.config");
+const bedrockConfig = require("../bedrock.config.js");
 
 // Configure tailwind screens from bedrock.config.js
 const screens = bedrockConfig.theme.breakpoints;
