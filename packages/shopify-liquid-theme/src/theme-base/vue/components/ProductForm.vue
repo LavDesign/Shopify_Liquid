8f3d3500b8e22ee45dc498ea4e13571445bd8e89
br.anderson <brian@banderson.io>
2023-02-03 10:35:19 -0500

    BL-42 Add basic form fields

--- packages/shopify-liquid-theme/src/theme-base/vue/components/ProductForm.vue
+++ packages/shopify-liquid-theme/src/theme-base/vue/components/ProductForm.vue
@@ -1,71 +1,86 @@
 <template>
-  <h1>Test</h1>
   <RaOptionPicker
-    label="size"
+    :label="Object.keys(optionsWithValues)[0]"
+    v-model="selectedOption1"
+    :options="getFormattedValuesByIndex(0)"
+  />
+  <RaOptionPicker
+    v-if="Object.keys(optionsWithValues).length > 1"
+    :label="Object.keys(optionsWithValues)[1]"
     size="lg"
-    :options="[
-      {
-        value: '10',
-        label: '10',
-      },
-    ]"
+    variant="dropdown"
+    v-model="selectedOption2"
+    :options="getFormattedValuesByIndex(1)"
+  />
+  <RaOptionPicker
+    v-if="Object.keys(optionsWithValues).length > 2"
+    :label="Object.keys(optionsWithValues)[2]"
+    size="lg"
+    variant="dropdown"
+    v-model="selectedOption3"
+    :options="getFormattedValuesByIndex(2)"
   />
 </template>
 
 <script setup>
 import { ref, computed } from "vue";
-// import axios from "axios";
-// import store from "../../raStore.js";
 import { RaOptionPicker } from "@bva/ui-vue";
 
 const props = defineProps({
   product: Object,
 });
 
-console.log(props);
-
-const quantity = ref(1)
+const quantity = ref(1);
 const availableQuantity = ref(0);
 const activeVariant = ref(null);
 const enableBuyInStore = ref(true);
 const enableBuyOnline = ref(true);
 const selectedVariant = ref();
-const selectedOption1 = ref();
-const selectedOption2 = ref();
+const selectedOption1 = ref("Small");
+const selectedOption2 = ref("Black");
 const selectedOption3 = ref();
 const isAddingToCart = ref(false);
 
+const optionsWithValues = props.product.options_with_values;
+
 const buttonText = computed(() => {
-  return this.isAddingToCart ? "Adding..." : "Add to Cart";
+  return isAddingToCart.value ? "Adding..." : "Add to Cart";
 });
 
+const hasComplexVariants = computed(() => {
+  return props.product ? props.product.options.length > 1 : false;
+});
+
+function getFormattedValuesByIndex(index) {
+  const key = Object.keys(optionsWithValues)[index];
+  return optionsWithValues[key].map((value) => ({
+    label: value,
+    value: value,
+  }));
+}
 
 const currentVariant = computed(() => {
-  let currentVariant = this.product.variants[0];
-  if (this.hasComplexVariants) {
-    if (this.product.options.length > 2) {
-      currentVariant = this.product.variants.find(
+  let currentVariant = props.product.variants[0];
+  if (hasComplexVariants.value) {
+    if (props.product.options.length > 2) {
+      currentVariant = props.product.variants.find(
         (variant) =>
-          variant.option1 === this.selectedOption1 &&
-          variant.option2 === this.selectedOption2 &&
-          variant.option3 === this.selectedOption3
+          variant.option1 === selectedOption1.value &&
+          variant.option2 === selectedOption2.value &&
+          variant.option3 === selectedOption3.value
       );
     } else {
-      currentVariant = this.product.variants.find(
+      currentVariant = props.product.variants.find(
         (variant) =>
-          variant.option1 === this.selectedOption1 &&
-          variant.option2 === this.selectedOption2
+          variant.option1 === selectedOption1.value &&
+          variant.option2 === selectedOption2.value
       );
     }
-  } else if (this.hasVariants) {
-    currentVariant = this.product.variants.find(
-      (variant) => variant.option1 === this.selectedOption1
+  } else if (props.product.variants_count > 1) {
+    currentVariant = props.product.variants.find(
+      (variant) => variant.option1 === selectedOption1.value
     );
   }
-  return currentVariant || this.product.variants[0];
-});
-
-const hasComplexVariant = computed(() => {
-  return this.product ? this.product.options.length > 1 : false;
+  return currentVariant || props.product.variants[0];
 });
 </script>
