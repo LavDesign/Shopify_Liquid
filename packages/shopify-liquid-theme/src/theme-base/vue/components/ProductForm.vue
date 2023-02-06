3c0b6353dcd281e57166d6126a8883472bdc7440
br.anderson <brian@banderson.io>
2023-02-06 11:55:34 -0500

    BL-42 WIP - Update form with components to swap variants

--- packages/shopify-liquid-theme/src/theme-base/vue/components/ProductForm.vue
+++ packages/shopify-liquid-theme/src/theme-base/vue/components/ProductForm.vue
@@ -1,47 +1,110 @@
 <template>
+  <!-- Loop through options -->
   <RaOptionPicker
-    :label="Object.keys(optionsWithValues)[0]"
-    v-model="selectedOption1"
-    :options="getFormattedValuesByIndex(0)"
+    :key="label"
+    v-for="(options, label) in formattedOptions"
+    :label="label"
+    :options="formattedOptions[label]"
+    :selected="selectedOptions[label]"
+    @change:option="
+      (selected, option) => handleOptionSelect(label, selected, option)
+    "
   />
-  <RaOptionPicker
+  <!-- Manually output all options for more granular control -->
+
+  <!-- <RaOptionPicker
+    :label="Object.keys(optionsWithValues)[0]"
+    :options="formattedOptions[0]"
+    :selected="selectedOptions[Object.keys(optionsWithValues)[0]]"
+    @change:option="
+      (selected, option) =>
+        handleOptionSelect(Object.keys(optionsWithValues)[0], selected, option)
+    "
+  /> -->
+
+  <!-- <RaOptionPicker
     v-if="Object.keys(optionsWithValues).length > 1"
     :label="Object.keys(optionsWithValues)[1]"
-    size="lg"
     variant="dropdown"
-    v-model="selectedOption2"
     :options="getFormattedValuesByIndex(1)"
-  />
-  <RaOptionPicker
+    :selected="selectedOptions[Object.keys(optionsWithValues)[1]]"
+    @change:option="handleDropdown"
+  /> -->
+
+  <!-- <RaOptionPicker
+    v-if="Object.keys(optionsWithValues).length > 1"
+    :label="Object.keys(optionsWithValues)[1]"
+    :options="getFormattedValuesByIndex(1)"
+    :selected="selectedOptions[Object.keys(optionsWithValues)[1]]"
+    @change:option="
+      (selected, option) =>
+        handleOptionSelect(Object.keys(optionsWithValues)[1], selected, option)
+    "
+  /> -->
+
+  <!-- <RaOptionPicker
     v-if="Object.keys(optionsWithValues).length > 2"
     :label="Object.keys(optionsWithValues)[2]"
     size="lg"
     variant="dropdown"
-    v-model="selectedOption3"
     :options="getFormattedValuesByIndex(2)"
-  />
+    @change:option="
+      (selected, selectedOption) =>
+        handleDropdown(
+          Object.keys(optionsWithValues)[0],
+          selected,
+          selectedOption
+        )
+    "
+  /> -->
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, reactive, computed } from "vue";
 import { RaOptionPicker } from "@bva/ui-vue";
 
 const props = defineProps({
   product: Object,
 });
 
-const quantity = ref(1);
-const availableQuantity = ref(0);
-const activeVariant = ref(null);
-const enableBuyInStore = ref(true);
-const enableBuyOnline = ref(true);
-const selectedVariant = ref();
-const selectedOption1 = ref("Small");
-const selectedOption2 = ref("Black");
-const selectedOption3 = ref();
-const isAddingToCart = ref(false);
+// ToDo:
 
-const optionsWithValues = props.product.options_with_values;
+// const quantity = ref(1);
+// const availableQuantity = ref(0);
+// const activeVariant = ref(null);
+// const enableBuyInStore = ref(true);
+// const enableBuyOnline = ref(true);
+// const selectedVariant = ref();
+
+const isAddingToCart = ref(false);
+const optionsWithValues = reactive(props.product.options_with_values);
+
+// Set initial options from first_available_variant
+const selectedOptions = reactive({});
+
+// Note: Insertion order should be preserved here as of ES2015 (assuming string keys),
+// but there could be edge cases where options might not be properly ordered if using an int as a key
+props.product.options.forEach((option, i) => {
+  selectedOptions[option] = props.product.first_available_variant.options[i];
+});
+
+// Set keyed object for all option values
+const formattedOptions = computed(() => {
+  const formattedOptions = {};
+
+  props.product.options.forEach((option) => {
+    formattedOptions[option] = [];
+    // console.log(optionsWithValues);
+    optionsWithValues[option].forEach((value) => {
+      formattedOptions[option].push({
+        label: value,
+        value,
+      });
+    });
+  });
+
+  return formattedOptions;
+});
 
 const buttonText = computed(() => {
   return isAddingToCart.value ? "Adding..." : "Add to Cart";
@@ -50,6 +113,17 @@ const buttonText = computed(() => {
 const hasComplexVariants = computed(() => {
   return props.product ? props.product.options.length > 1 : false;
 });
+
+const handleOptionSelect = (optionKey, selected, selectedOption) => {
+  selectedOptions[optionKey] = selectedOption.value;
+};
+
+// // Currently Dropdown variation of the optionPicker component is firing events multiple times with different values
+// const handleDropdown = (selected, selectedOption) => {
+//   console.log(selected);
+//   console.log(selectedOption);
+//   // selectedOptions[optionKey] = selectedOption.value;
+// };
 
 function getFormattedValuesByIndex(index) {
   const key = Object.keys(optionsWithValues)[index];
@@ -65,22 +139,22 @@ const currentVariant = computed(() => {
     if (props.product.options.length > 2) {
       currentVariant = props.product.variants.find(
         (variant) =>
-          variant.option1 === selectedOption1.value &&
-          variant.option2 === selectedOption2.value &&
-          variant.option3 === selectedOption3.value
+          variant.option1 === Object.values(selectedOptions)[0] &&
+          variant.option2 === Object.values(selectedOptions)[1] &&
+          variant.option3 === Object.values(selectedOptions)[2]
       );
     } else {
       currentVariant = props.product.variants.find(
         (variant) =>
-          variant.option1 === selectedOption1.value &&
-          variant.option2 === selectedOption2.value
+          variant.option1 === Object.values(selectedOptions)[0] &&
+          variant.option2 === Object.values(selectedOptions)[1]
       );
     }
   } else if (props.product.variants_count > 1) {
     currentVariant = props.product.variants.find(
-      (variant) => variant.option1 === selectedOption1.value
+      (variant) => variant.option1 === Object.values(selectedOptions)[0]
     );
   }
-  return currentVariant || props.product.variants[0];
+  return currentVariant || props.product.first_available_variant;
 });
 </script>
