9b8c1f3304a50a1ee59a6f839d0640368177c14e
Servio <servio.mora19@gmail.com>
2023-02-09 00:03:21 -0500

    BL-44 slider setup with snippet passed down as slot

--- packages/shopify-liquid-theme/src/theme-base/vue/components/ContentCarousel.vue
+++ packages/shopify-liquid-theme/src/theme-base/vue/components/ContentCarousel.vue
@@ -1,29 +1,49 @@
 <template>
-  <swiper
-  :slides-per-view="3"
-  :space-between="50">
-    <slot></slot>
-  </swiper>
+  <div ref="carousel">
+    <swiper
+      :slides-per-view="2"
+      :space-between="20"
+      navigation
+      @swiper="onSwiper"
+      @slideChange="onSlideChange">
+      <slot></slot>
+    </swiper>
+  </div>
 </template>
 
-<script>
-import { Swiper, SwiperSlide } from 'swiper/vue';
+<script setup>
+  import { ref, computed, onMounted } from "vue";
+  import { Navigation, Swiper, SwiperSlide } from 'swiper/vue';
 
-export default {
-  name: "ContentCarousel",
-  components: {
-    Swiper,
-    SwiperSlide,
-  },
-  props: {},
-  data() {
-    return {
-      data: "Test",
-    };
-  },
-  computed: {},
-  mounted: {},
-};
+  import 'swiper/css';
+
+  const props = defineProps({
+    options: Object,
+  });
+
+  const carousel = ref(null)
+
+  const onSwiper = (swiper) => {
+    console.log(swiper);
+  };
+  const onSlideChange = () => {
+    console.log('slide change');
+  };
+
+  const navigation = {
+    nextEl: '.ra-carousel__nav-cta--next',
+    prevEl: '.ra-carousel__nav-cta--prev',
+  }
+
+  onMounted(() => {
+    const swiperWrapper = carousel.value.querySelector('.swiper-wrapper');
+    const slides = carousel.value.querySelectorAll('.swiper-slide');
+
+    slides.forEach(slide => {
+      swiperWrapper.appendChild(slide);
+    });
+  });
+
 </script>
 
 <style lang="scss" scoped></style>
