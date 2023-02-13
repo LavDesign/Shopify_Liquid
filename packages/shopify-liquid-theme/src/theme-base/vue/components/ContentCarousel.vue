6bea2d6aa6da37462753ad9ddc3fcc00f1138ae9
Servio <servio.mora19@gmail.com>
2023-02-13 15:49:09 -0500

    BL-44 troubleshooting slots

--- packages/shopify-liquid-theme/src/theme-base/vue/components/ContentCarousel.vue
+++ packages/shopify-liquid-theme/src/theme-base/vue/components/ContentCarousel.vue
@@ -3,47 +3,63 @@
     <swiper
       :slides-per-view="2"
       :space-between="20"
-      navigation
+      :navigation="navigation"
       @swiper="onSwiper"
-      @slideChange="onSlideChange">
+      @slideChange="onSlideChange"
+      :breakpoints="breakpoints"
+    >
       <slot></slot>
     </swiper>
   </div>
 </template>
 
 <script setup>
-  import { ref, computed, onMounted } from "vue";
-  import { Navigation, Swiper, SwiperSlide } from 'swiper/vue';
+import { ref, computed, onMounted } from "vue";
+import { Swiper, Navigation } from 'swiper/vue';
 
-  import 'swiper/css';
+import "swiper/css";
 
-  const props = defineProps({
-    options: Object,
+const props = defineProps({
+  options: Object,
+});
+
+const carousel = ref(null)
+
+const onSwiper = (swiper) => {
+  console.log(swiper);
+};
+const onSlideChange = () => {
+  console.log('slide change');
+};
+
+const breakpoints = {
+  640: {
+    slidesPerView: 2,
+    spaceBetween: 15,
+  },
+  768: {
+    slidesPerView: 3,
+    spaceBetween: 24,
+  },
+  1024: {
+    slidesPerView: 4,
+    spaceBetween: 40,
+  },
+};
+
+const navigation = {
+  nextEl: '.ra-carousel__nav-cta--next',
+  prevEl: '.ra-carousel__nav-cta--prev',
+}
+
+onMounted(() => {
+  const swiperWrapper = carousel.value.querySelector('.swiper-wrapper');
+  const slides = carousel.value.querySelectorAll('.swiper-slide');
+
+  slides.forEach((slide) => {
+    swiperWrapper.appendChild(slide);
   });
-
-  const carousel = ref(null)
-
-  const onSwiper = (swiper) => {
-    console.log(swiper);
-  };
-  const onSlideChange = () => {
-    console.log('slide change');
-  };
-
-  const navigation = {
-    nextEl: '.ra-carousel__nav-cta--next',
-    prevEl: '.ra-carousel__nav-cta--prev',
-  }
-
-  onMounted(() => {
-    const swiperWrapper = carousel.value.querySelector('.swiper-wrapper');
-    const slides = carousel.value.querySelectorAll('.swiper-slide');
-
-    slides.forEach(slide => {
-      swiperWrapper.appendChild(slide);
-    });
-  });
-
+});
 </script>
 
 <style lang="scss" scoped></style>
