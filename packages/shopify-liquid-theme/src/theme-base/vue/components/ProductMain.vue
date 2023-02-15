70a48d3c1f0b3ce9afbe81d5905ad191fd86a215
br.anderson <brian@banderson.io>
2023-02-15 12:14:12 -0500

    BL-42 Update product form with review slot

--- packages/shopify-liquid-theme/src/theme-base/vue/components/ProductMain.vue
+++ packages/shopify-liquid-theme/src/theme-base/vue/components/ProductMain.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="flex flex-row justify-between mb-3">
-    <h1 class="h4">{{ product.title }}</h1>
+    <!-- <h1 class="h4">{{ product.title }}</h1> -->
     <div class="h4" v-if="currentVariant">
       {{ $filters.money(currentVariant.price) }}
     </div>
   </div>
+  <slot name="review-stars"></slot>
   <ProductForm v-bind="{ product }" />
 </template>
 
 <script setup>
-import { toRefs } from "vue";
+import { toRefs, useSlots } from "vue";
 import ProductForm from "./ProductForm.vue";
 import { useProductPageStore } from "../stores/productPage";
 
@@ -19,4 +20,9 @@ const { currentVariant } = toRefs(productStore);
 const props = defineProps({
   product: Object,
 });
+
+const slots = useSlots();
+if (slots["review-stars"]) {
+  console.log("has slot");
+}
 </script>
