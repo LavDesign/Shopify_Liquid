7de0d67e24bf83c5583888abccc511a113db4387
ghostego <alexfindlayjohnson@gmail.com>
2023-03-02 13:41:28 -0800

    adjust empty cart to use linklist

--- packages/shopify-liquid-theme/src/theme-base/vue/components/CartComponents/EmptyCart.vue
+++ packages/shopify-liquid-theme/src/theme-base/vue/components/CartComponents/EmptyCart.vue
@@ -4,9 +4,9 @@
     <template v-for="(cta, i) in ctas" :key="`cta-${i}`">
       <a
         class="ra-button ra-button ra-button--secondary ra-button--lg mb-2"
-        :href="cta.empty_cart_cta_link"
+        :href="cta.url"
       >
-        {{ cta.empty_cart_cta_text }}
+        {{ cta.title }}
       </a>
     </template>
   </div>
@@ -26,21 +26,5 @@ const settings = computed(() => props.settings);
 
 const header = computed(() => settings.value.empty_cart_header);
 
-const ctas = computed(() => {
-  const cta_keys =
-    "enable_empty_cart_cta,empty_cart_cta_text,empty_cart_cta_link".split(",");
-  const ctaArray = [];
-
-  [1, 2, 3].forEach((i) => {
-    const tempObject = {};
-    cta_keys.forEach((key) => {
-      const tempKey = key + "_" + i;
-      if (tempKey && settings.value[tempKey]) {
-        tempObject[key] = settings.value[tempKey];
-      }
-    });
-    if (Object.keys(tempObject)?.length > 1) ctaArray.push(tempObject);
-  });
-  return ctaArray;
-});
+const ctas = computed(() => settings.value?.links);
 </script>
