2a691525a96691076d0fa0b3024506629e8f43b7
ghostego <alexfindlayjohnson@gmail.com>
2023-03-16 16:03:04 -0700

    first pass removal

--- packages/shopify-liquid-theme/src/theme-base/vue/components/index.js
+++ packages/shopify-liquid-theme/src/theme-base/vue/components/index.js
@@ -1,10 +1,8 @@
-import ButtonCounter from "./ButtonCounter.vue";
 import ProductMain from "./ProductMain.vue";
 import CartPage from "./CartPage.vue";
 import CartInline from "./CartInline.vue";
 
 export default {
-  ButtonCounter,
   ProductMain,
   CartPage,
   CartInline,
