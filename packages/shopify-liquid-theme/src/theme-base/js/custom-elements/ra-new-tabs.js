dc2f9bac62aff587c4bcc71b35455e6acfeb497b
ghostego <alexfindlayjohnson@gmail.com>
2023-04-18 08:26:05 -0700

    BL-313: continue refactor of tabs

--- packages/shopify-liquid-theme/src/theme-base/js/custom-elements/ra-new-tabs.js
+++ packages/shopify-liquid-theme/src/theme-base/js/custom-elements/ra-new-tabs.js
@@ -1,50 +1,34 @@
 export default class RaNewTabs extends HTMLElement {
   constructor() {
     super();
-    this.externalToggle = this.getAttribute("external-toggle");
-    this.id = this.getAttribute("id");
-    this.tabButtons = document.querySelectorAll("[data-tab]");
-    this.tabContainer = document.querySelector("[data-tab-container]");
-    this.animating = false;
+    this.tabButtons = this.querySelectorAll("[data-tab]");
+    this.tabContainer = this.querySelector("[data-tab-container]");
+    this.tabPanels = this.tabContainer.querySelectorAll("[data-tab-target]");
 
     this.tabButtons.forEach((button) => {
       button.addEventListener("click", () => {
-        if (this.animating || button.classList.contains("active")) return false;
-
+        if (button.classList.contains("active")) return false;
         const tabId = button.dataset.tab;
 
         // Hide all the tab panels
-        const tabPanels = document.querySelectorAll(".tab-panel");
-        const selectedTabPanel = document.querySelector(
-          `.tab-panel[data-tab="${tabId}"]`
+        const selectedTabPanel = this.tabContainer.querySelector(
+          `[data-tab-target="${tabId}"]`
         );
-        tabPanels.forEach((panel) => {
+        this.tabPanels.forEach((panel) => {
           if (
             panel != selectedTabPanel &&
             !panel.classList.contains("hidden")
           ) {
-            panel.classList.toggle("closing");
-            setTimeout(() => {
-              panel.classList.toggle("hidden");
-              panel.classList.toggle("closing");
-            }, 250);
+            panel.classList.toggle("hidden");
           }
         });
-        setTimeout(() => {
-          selectedTabPanel.classList.toggle("opening");
-          selectedTabPanel.classList.toggle("hidden");
-        }, 250);
-
-        setTimeout(() => {
-          selectedTabPanel.classList.toggle("opening");
-        }, 750);
+        selectedTabPanel.classList.toggle("hidden");
 
         // Set the active state on the selected tab button
         this.tabButtons.forEach((button) => {
           button.classList.remove("text-grey-900", "active");
         });
         button.classList.add("text-grey-900", "active");
-        this.animating = false;
       });
     });
   }
