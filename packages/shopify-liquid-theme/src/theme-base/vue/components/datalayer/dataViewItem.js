1b44444ea27f2da5defcdfc48cef4886b90011c2
ghostego <alexfindlayjohnson@gmail.com>
2023-04-27 15:08:25 -0700

    BL-136: view_item event

--- packages/shopify-liquid-theme/src/theme-base/vue/components/datalayer/dataViewItem.js
+++ packages/shopify-liquid-theme/src/theme-base/vue/components/datalayer/dataViewItem.js
@@ -1,6 +1,10 @@
 import { itemList, selectedOptions } from "./utilities";
 
 export function dataViewItem(product, variant) {
+  console.log("Logging Data View");
+  console.log(product);
+  console.log(variant.value);
+  console.log("---------------");
 
   dataLayer.push({ ecommerce: null });
   dataLayer.push({
@@ -9,10 +13,11 @@ export function dataViewItem(product, variant) {
       new Date().getTime() + "." + Math.random().toString(36).substring(5),
     ecommerce: {
       currency: Shopify.currency.active,
-      value: variant.value.price,
+      value: (variant.value.price / 100).toFixed(2),
       items: [
         {
           item_id: variant.value.id,
+          item_featured_image: null,
           shopify_product_id: product.id,
           shopify_variant_id: variant.value.id,
           shopify_sku: variant.value.sku,
@@ -26,8 +31,11 @@ export function dataViewItem(product, variant) {
           item_variant: product.has_only_default_variant
             ? product.title
             : variant.value.title,
-          price: variant.value.price,
-          ...selectedOptions(product, variant),
+          price: (variant.value.price / 100).toFixed(2),
+          option_one: variant.value.option1,
+          option_two: variant.value.option2,
+          option_three: variant.value.option3,
+          quantity: 1,
           ...itemList(product, variant),
         },
       ],
