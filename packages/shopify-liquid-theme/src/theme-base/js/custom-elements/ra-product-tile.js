00124aebb609b78c0c6c0d276e89140fcf0a9bd1
ghostego <alexfindlayjohnson@gmail.com>
2023-05-05 14:58:31 -0700

    BL-401: init swatch support

---
+++ packages/shopify-liquid-theme/src/theme-base/js/custom-elements/ra-product-tile.js
@@ -0,0 +1,46 @@
+export default class RaProductTile extends HTMLElement {
+  constructor() {
+    super();
+    this.product = JSON.parse(this.getAttribute("data-product"));
+    this.featuredImage = this.querySelector("[data-featured-image]");
+    this.altImage = this.querySelector("[data-alt-image]");
+    this.variants = this.product.variants;
+    this.currentVariant;
+    this.productOptions = this.querySelectorAll("[data-option-value]");
+    this.productUrl = this.querySelector("[data-product-link]");
+  }
+
+  connectedCallback() {
+    this.currentVariant = this.product.variants.find(
+      (variant) => variant.id == this.getAttribute("data-current-variant")
+    );
+    this.productOptions.forEach((option) => {
+      option.addEventListener("click", this.swatchClick.bind(this));
+    });
+  }
+
+  swatchClick(e) {
+    const activeOption = e.target;
+    if (activeOption.classList.contains("active")) return false;
+    Array.from(activeOption.parentElement.children).forEach((option) => {
+      option.classList.remove("active");
+    });
+    activeOption.classList.add("active");
+    const { optionPosition, optionValue } = activeOption.dataset;
+    const newOptions = this.currentVariant.options.slice();
+    newOptions[optionPosition - 1] = optionValue;
+    const newVariant = this.product.variants.find((variant) =>
+      variant.options.every((value, index) => value === newOptions[index])
+    );
+    this.setCurrentVariant(newVariant);
+  }
+
+  setCurrentVariant(variant) {
+    this.currentVariant = variant;
+    let productUrl = `/products/${this.product.handle}`;
+    if (this.currentVariant?.id) {
+      productUrl += `?variant=${this.currentVariant}`;
+    }
+    this.productUrl.setAttribute("href", productUrl);
+  }
+}
