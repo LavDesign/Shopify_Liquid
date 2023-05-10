6e39d1fbe353c5e250faf2e1abf8a29d8c50a221
br.anderson <brian@banderson.io>
2023-05-10 13:59:08 -0400

    Add swatch spacing props

--- packages/shopify-liquid-theme/src/theme-base/vue/components/OptionPicker.vue
+++ packages/shopify-liquid-theme/src/theme-base/vue/components/OptionPicker.vue
@@ -1,5 +1,5 @@
 <template>
-  <RaOptionPicker v-bind="$attrs" class="product-form__option-picker mb-6" />
+  <RaOptionPicker v-bind="$attrs" class="product-form__option-picker mb-6" gap="16px" />
 </template>
 
 <script setup>
