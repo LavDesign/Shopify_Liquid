0cb976749cce08bdd1d686bca41c72c876217f4d
Jeff Ginsburg <jeff.ginsburg@thestable.com>
2023-05-18 15:22:24 -0400

    BL-49 bazaarvoice implementation

--- packages/shopify-liquid-theme/src/theme-base/vue/components/index.js
+++ packages/shopify-liquid-theme/src/theme-base/vue/components/index.js
@@ -1,9 +1,11 @@
 import ProductMain from "./ProductMain.vue";
 import CartPage from "./CartPage.vue";
 import CartInline from "./CartInline.vue";
+import BazaarVoiceMainWidget from "./BazaarVoiceMainWidget.vue";
 
 export default {
   ProductMain,
   CartPage,
   CartInline,
+  BazaarVoiceMainWidget,
 };
